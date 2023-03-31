94fd66c11f2ab16067eba057e343e42527ab03ef
Oleksandr Omelyan <jemm.ua@gmail.com>
2023-03-31 17:36:49 +0300

    general

--- src/main/java/exercises/Main.java
+++ src/main/java/exercises/Main.java
@@ -3,12 +3,21 @@ package exercises;
 public class Main {
     public static void main(String[] args) {
         One one = new One();
-        Echo two = new Echo();
+        Echo echo = new Echo();
 
         one.print(1);
         one.print("One");
 
-        two.print(1);
-        two.print("One");
+        echo.print(1);
+        echo.print("One");
+
+        {
+            int x = 10;
+            System.out.println(x);
+        }
+        int x = 3;
+        System.out.println(x);
     }
+
+
 }
