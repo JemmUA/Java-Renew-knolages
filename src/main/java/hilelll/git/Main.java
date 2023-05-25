6c7d673cb28c4d769b42db79d8bab88ef6104ed8
Oleksandr Omelyan <jemm.ua@gmail.com>
2023-05-25 22:46:58 +0300

    sql

--- src/main/java/hilelll/git/Main.java
+++ src/main/java/hilelll/git/Main.java
@@ -1,4 +1,51 @@
 package hilelll.git;
 
 public class Main {
+/**
+    git remote add origin https://github.com/JemmUA/tryGit.git
+
+    git init
+    git add .
+    git add a
+    git fetch
+    git push origin develop
+    git pull origin develop
+    git checkout - (на попередню гілку)
+    git checkout (перейти на гілку)
+    git checkout -b (створити та перейти на гілку)
+    git branch (поточна гілка)
+    git branch xxx(створити нову гілку)
+    git restore .\src\a (відновити файл а)
+    git restore . (відновити всі файли)
+    git commit -m "file with changes brabch" (коміт)
+    git commit -am "file with changes brabch" (коміт з індексфцією
+    git merge main
+    git log -5 --oneline
+    git merge -- abort
+
+            configurations
+    git config --system -- list
+    git config --global -- list
+    git config --local -- list
+
+            stash
+    git stash
+    git stash save develop
+    git stash list
+    git stash apply 0
+    git stash pop 0
+    git stash drop 0
+            --index
+
+    git commit --amend (для останнього комміта)
+
+    cherry-pick
+    git cherry-pick
+    git cherry-pick main develop
+    git log -11 --oneline
+    git cherry-pick eff7736 --no-commit
+
+
+*/
+
 }
---
+++ src/main/java/hilelll/sql/Main.java
@@ -0,0 +1,104 @@
+package hilelll.sql;
+
+public class Main {
+    /**
+     * DataBaseManagementSystem (DBMS/СУБД)
+     * RelationalDataBaseManagementSystem (RDBMS/РСУБД)
+     *
+     * Types of databases:
+         * 1) Centralized Database.
+         * 2) Distributed Database.
+         * 3) Relational Database.
+         * 4) NoSQL Database (not only sql).
+         * 5) Cloud Database.
+         * 6) Object-oriented Databases.
+         * 7) Hierarchical Databases.
+         * 8) Network Databases.
+     *
+    * */
+
+    /**
+     * SQL
+     * Structured query language (структурована мова запитів)
+     *
+     * DDL
+     * Data definition language
+     * CREATE, ALTER (modification), DROP
+     * (operations with tables: create, modify fields, delete)
+     *
+     *The ALTER TABLE statement is used to add, delete,
+     * or modify columns in an existing table.
+     *
+     * The ALTER TABLE statement is also used to add and drop
+     * various constraints on an existing table.
+     *
+     * Examples rename and change type:
+     *      ALTER TABLE racing.drivers
+     *      RENAME COLUMN top3 TO top20;
+     *
+     *      ALTER TABLE racing.drivers
+     *      ALTER COLUMN top5 TYPE int USING top5::integer;
+     *
+     * DML
+     * Data manipulation language
+     * (operations with data)
+     * SELECT, INSERT, UPDATE, DELETE
+     * INSERT INTO table ( ,  , ) VAL   UES ( , , );
+     *
+     * DCL
+     * Data control language
+     * (permissions)
+     * GRANT, REVOKE, DENY
+     *
+     * TCL
+     * Transaction control language
+     * BEGIN TRANSACTION
+     * COMMIT TRANSACTION
+     * ROLLBACK TRANSACTION
+     * SAVE TRANSACTION
+     *
+     *
+     * Troubles of data management:
+     *
+     * cascade deleting with links:
+     *
+     * create table racing.driver_status (
+     * 	    driver_id int REFERENCES racing.drivers (id) ON DELETE CASCADE,
+     * 	    status_id int REFERENCES racing.statuses (id) ON DELETE CASCADE
+     * )
+     *
+     * restriction deleting if exist links:
+     *
+     * create table racing.driver_status (
+     *      driver_id int, constraint fk_driver
+     *              FOREIGN KEY (driver_id) REFERENCES racing.drivers (id),
+     *      status_id int, constraint fk_status
+     *              FOREIGN KEY (status_id) REFERENCES racing.statuses (id)
+     * */
+
+    /** CRUD operations
+     *
+     * C    INSERT INTO table (id, name) VALUES (1, 'ALEX');
+     * R    SELECT * FROM table;
+     * U    UPDATE table SET column_name = 20;
+     * D    DELETE FROM table;
+     *
+     * WARNING!
+     * Update without conditions will update WHOLE COLUMN with new value!
+     *
+     * Conditional operators:
+     * WHERE: AND OR IN NOT
+     *
+     *
+     * JOIN
+     * INNER JOIN, LEFT JOIN, RIGHT JOIN, FULL JOIN
+     *
+     * SELECT d.name, d.email, d.wins, s.status as stat FROM racing.drivers as d
+     * inner join racing.driver_status ds ON d.id = ds.driver_id
+     * inner join racing.statuses s ON s.id = ds.status_id
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
